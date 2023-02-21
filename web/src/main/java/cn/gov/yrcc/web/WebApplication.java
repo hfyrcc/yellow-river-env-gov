5ce7ed072f7aa38c4e7c12bb43ea466464039082
wylu <wylu@zkjg.com>
2023-02-21 08:44:39 +0800

    chore: add module

---
+++ web/src/main/java/cn/gov/yrcc/web/WebApplication.java
@@ -0,0 +1,18 @@
+package cn.gov.yrcc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * web application
+ *
+ * @Author wylu
+ * @Date 2023/2/21 上午 08:24
+ */
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+}
---
+++ web/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module web {
+	// 以下是导出一方包模块
+
+	// 以下是导入三方包模块
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+}
